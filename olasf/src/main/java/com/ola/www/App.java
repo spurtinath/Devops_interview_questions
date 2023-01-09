14b7ae76472c2a72c9c1b844121baef15605925a
sps <you@example.com>
2023-01-09 17:10:42 +0530

    created a new project

---
+++ olasf/src/main/java/com/ola/www/App.java
@@ -0,0 +1,13 @@
+package com.ola.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
